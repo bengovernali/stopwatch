be6175893b6c279ceab03e230f144954699c8a60
Ben Governali <bgovern93@gmail.com>
2019-06-27 11:43:35 -0400

    start, stop and reset buttons functional

--- src/App.js
+++ src/App.js
@@ -3,37 +3,45 @@ import './App.css';
 import ElapsedTime from './elapsed-time.js';
 import Start from './start.js';
 import Stop from './stop.js';
+import Reset from './reset.js';
+
 
 class App extends Component {
-  state = { counter: 0, run: false };
+  state = { time: 0 };
+  
+  counter = 0;
+
+  updateTime() {
+    const newTime = this.state.time + 1;
+    this.setState(() => {
+      return {time: newTime};
+    })
+  }
   
   start() {
-    const startTime = new Date();
-    this.setState(() => {
-      return {run: true };
-    });
-    while (this.state.run === true) {
-      let time = (new Date()) - startTime;
-      console.log(time);
-      this.props.time = time;
-      this.setState(() => {
-        return {counter: time};
-      });
-    }
-  }
+    this.counter = setInterval(() => {
+      this.updateTime()}, 1000);
+    return this.counter;
+  };
 
   stop() {
+    clearInterval(this.counter)
+  }
+
+  reset() {
+    this.stop();
     this.setState(() => {
-      return {run: false};
-    });
+      return {time: 0};
+    })
   }
   
   render() {
     return (
       <div className="App">
-        <ElapsedTime />
-        <Start />
-        <Stop />
+        <ElapsedTime time={this.state.time} />
+        <Start start={() => this.start()} />
+        <Stop stop={() => this.stop()} />
+        <Reset reset={() => this.reset()} />
       </div>
     )
   };
